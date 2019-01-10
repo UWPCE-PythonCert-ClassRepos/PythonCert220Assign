ebfcf6c4d7490aa4088c5794e84bd116cd8bf48e
D Smith <dsmith82@uw.edu>
2019-01-09 22:23:16 -0800

    in class activity code

---
+++ students/DrewSmith/lesson01/activity/calculator/divider.py
@@ -0,0 +1,16 @@
+''' Divider Module '''
+
+
+# pylint: disable=R0903
+class Divider:
+    ''' Represents adding function '''
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        '''
+        Divide two numbers
+
+        :param operand_1: first number
+        :param operand_2: second number
+        '''
+        return operand_1/operand_2
