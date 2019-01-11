241eb2aeac24691ef4acbb348bec19b2f0d4b9fb
Kyle Barry <kylesbarry@gmail.com>
2019-01-10 20:58:57 -0800

    Made the majority of pylint changes in these two files

--- students/KyleBarry/lesson01/assignment/inventory_management/inventoryClass.py
+++ students/KyleBarry/lesson01/assignment/inventory_management/inventoryClass.py
@@ -1,17 +1,21 @@
-# Inventory class
-class inventory:
-
-    def __init__(self, productCode, description, marketPrice, rentalPrice):
-        self.productCode = productCode
+class Inventory:
+    """Class to hold inventory details"""
+    def __init__(self, product_code, description, market_price, rental_price):
+        """Establish variables for inventory"""
+        self.product_code = product_code
         self.description = description
-        self.marketPrice = marketPrice
-        self.rentalPrice = rentalPrice
+        self.market_price = market_price
+        self.rental_price = rental_price
 
-    def returnAsDictionary(self):
-        outputDict = {}
-        outputDict['productCode'] = self.productCode
-        outputDict['description'] = self.description
-        outputDict['marketPrice'] = self.marketPrice
-        outputDict['rentalPrice'] = self.rentalPrice
+    def return_as_dictionary(self):
+        """Return dictionary with inventory class variables"""
+        output_dict = {}
+        output_dict['product_code'] = self.product_code
+        output_dict['description'] = self.description
+        output_dict['market_price'] = self.market_price
+        output_dict['rental_price'] = self.rental_price
 
-        return outputDict
+        return output_dict
+
+    def extra_method(self):
+        """Nonsene method for class"""
