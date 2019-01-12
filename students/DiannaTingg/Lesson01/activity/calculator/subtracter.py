b9be2ebbe3dce0d5149087a97eef6e9c8f0d3299
Dianna Tingg <dtingg@uw.edu>
2019-01-11 20:14:05 -0800

    added lesson 1 activity

---
+++ students/DiannaTingg/Lesson01/activity/calculator/subtracter.py
@@ -0,0 +1,19 @@
+"""
+This module provides a subtraction operator
+"""
+
+
+class Subtracter():
+    """
+    Returns the difference of two numbers
+    """
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """
+        Subtracts one number from the other
+        :param operand_1: number
+        :param operand_2: number
+        :return: Quotient of two numbers
+        """
+        return operand_1 - operand_2
