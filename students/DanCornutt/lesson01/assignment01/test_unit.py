9f0288dcdcd51010d6c89e5dd4318f0021373a97
Dan Cornutt <dacornutt@gmail.com>
2019-01-14 21:23:23 -0800

    Added test_unit.py file

---
+++ students/DanCornutt/lesson01/assignment01/test_unit.py
@@ -0,0 +1,6 @@
+"""Test module for inventory mangement dir.
+    All classes have unit tests for test driven dev."""
+
+import sys
+from inventory_management import market_prices, inventory_class,
+furniture_class, electric_appliances_class
