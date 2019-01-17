774a181fd514950eb6ea272ee29a620c34fa7de9
Jae Kim <kjae>
2019-01-16 17:19:59 -0800

    Removed commit of .gitignore

--- students/JaeKim/lesson01/assignment/inventory/electric_appliance.py
+++ students/JaeKim/lesson01/assignment/inventory/electric_appliance.py
@@ -9,7 +9,11 @@ class ElectricAppliance(Inventory):
     """
     Electric appliances class
     """
+
+    # Replace using kwargs for too many arguments as an option
     def __init__(self, product_code, description, market_price, rental_price, brand, voltage):
+
+        #super().__init__(args)
         Inventory.__init__(self, product_code, description, market_price, rental_price)
 
         self.brand = brand
