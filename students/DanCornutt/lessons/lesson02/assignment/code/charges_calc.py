3e10f9be5bce63d39b420474e4418ae813f158b1
Dan Cornutt <dacornutt@gmail.com>
2019-01-21 19:13:56 -0800

    got debug levels working, need to finalize log requirements

--- students/DanCornutt/lessons/lesson02/assignment/code/charges_calc.py
+++ students/DanCornutt/lessons/lesson02/assignment/code/charges_calc.py
@@ -6,35 +6,54 @@ import json
 import datetime
 import math
 import logging
+import sys
 
-log_format = ("%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s \
+
+def log_debug_setup(debug_lvl):
+    """Function sets up logging and debugging for script.
+    param2: debug level"""
+    log_format = ("%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s \
     %(message)s")
-formatter = logging.Formatter(log_format)
+    formatter = logging.Formatter(log_format)
 
-file_handler = logging.FileHandler('changes_calc.log')
-file_handler.setLevel(logging.WARNING)
-file_handler.setFormatter(formatter)
+    file_handler = logging.FileHandler('changes_calc.log')
+    file_handler.setFormatter(formatter)
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-console_handler.setFormatter(formatter)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    if debug_lvl == "0":
+        # debug_lvl 0: no debug or log file
+        logger.disabled = True
+    elif debug_lvl == "1":
+        # debug_lvl 1: Only error messages
+        file_handler.setLevel(logging.ERROR)
+        console_handler.setLevel(logging.ERROR)
+    elif debug_lvl == "2":
+        # debug_lvl 2: Error messags and warnings
+        file_handler.setLevel(logging.WARNING)
+        console_handler.setLevel(logging.WARNING)
+    elif debug_lvl == "3":
+        # debug_lvl 3: Error messages, warnings, and debug
+        console_handler.setLevel(logging.DEBUG)
+        file_handler.setLevel(logging.DEBUG)
+    return logger
 
 
 def parse_cmd_arguments():
     """Arguments for CLI"""
     parser = argparse.ArgumentParser(description='Process some integers.')
-    subparsers = parser.add_subparsers()
     parser.add_argument(
         '-i', '--input', help='input JSON file', required=True)
     parser.add_argument(
         '-o', '--output', help='ouput JSON file', required=True)
     parser.add_argument(
-        '-d', '--debug', help='debug mode')
+        '-d', '--debug', help='debug mode', choices=['0', '1', '2', '3'])
 
     return parser.parse_args()
 
@@ -44,20 +63,20 @@ def load_rentals_file(filename):
         try:
             data = json.load(file)
         except FileNotFoundError:
-            logger.error('No file "{}" available to load.'.format(filename))
+            logs.error('No file "{}" available to load.'.format(filename))
             exit(0)
     return data
 
 
 def calculate_additional_fields(data):
     for value in data.values():
-        logger.debug("Calculating fields for {} item".format(
+        logs.debug("Calculating fields for {} item".format(
             value['product_code']))
         try:
             rental_start = datetime.datetime.strptime(value['rental_start'],
                                                       '%m/%d/%y')
             if value['rental_end'] == "":
-                logger.warning('rental_end is null')
+                logs.warning('rental_end is null')
                 rental_end = datetime.datetime.now()
             else:
                 rental_end = datetime.datetime.strptime(value['rental_end'],
@@ -66,13 +85,13 @@ def calculate_additional_fields(data):
             value['total_price'] = value['total_days'] * value['price_per_day']
             value['sqrt_total_price'] = math.sqrt(abs(value['total_price']))
             if value['units_rented'] < 1:
-                logger.warning('units rented less than 1')
+                logs.warning('units rented less than 1')
                 value['unit_cost'] = value['total_price']
-                logger.error("Math failre. Setting unit cost equal to total \
+                logs.error("Math failure. Setting unit cost equal to total \
                     price.")
             else:
-                value['unit_cost'] = \
-                    value['total_price'] / value['units_rented']
+                value['unit_cost'] = (value['total_price'] /
+                                      value['units_rented'])
         except:
             exit(0)
 
@@ -86,7 +105,7 @@ def save_to_json(filename, data):
 
 if __name__ == "__main__":
     args = parse_cmd_arguments()
-    # log_debug_setup('changes_calc.log', args.debug)
+    logs = log_debug_setup(args.debug)
     data = load_rentals_file(args.input)
     data = calculate_additional_fields(data)
     save_to_json(args.output, data)
