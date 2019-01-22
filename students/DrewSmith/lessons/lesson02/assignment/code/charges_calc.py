dae680863d4f9870b8a05d00dd155d37af31ff48
D Smith <dsmith82@uw.edu>
2019-01-21 18:46:57 -0800

    seperate some data checks into their own try-catch blocks

--- students/DrewSmith/lessons/lesson02/assignment/code/charges_calc.py
+++ students/DrewSmith/lessons/lesson02/assignment/code/charges_calc.py
@@ -88,34 +88,54 @@ def calculate_additional_fields(input_data):
 
     :param data: dictionary of source file values
     '''
-
+    key_log = "Invalid data: {0}: missing {1} value: {2}"
     for key, value in input_data.items():
         try:
             rental_start = datetime.datetime.strptime(
                 value['rental_start'], '%m/%d/%y')
             rental_end = datetime.datetime.strptime(
                 value['rental_end'], '%m/%d/%y')            
-
-            value['total_days'] = (rental_end - rental_start).days
-
-            if value['total_days'] < 0:
-                logging.error(f"Invalid data: {key} rental start " + \
-                    f"{rental_start} is greater than the end {rental_end}")
-                print(f"ERROR: {key} rental start is greater than end")
-                continue
-
-            value['total_price'] = value['total_days'] * value['price_per_day']
-            value['sqrt_total_price'] = math.sqrt(value['total_price'])
-            value['unit_cost'] = value['total_price'] / value['units_rented']
-        except KeyError as key_error:
-            logging.warning(f"Invalid data: {key}: missing " + \
-                f"{key_error.args[0]} value: {str(key_error)}")
-            print(f"WARNING: {key} missing {key_error.args[0]} value")
+        except KeyError as key_err:
+            logging.warn(key_log.format(key, key_err.args[0], str(key_err)))
+            print(f"WARNING: {key} missing {key_err.args[0]} value")
         except ValueError as value_error:
-            logging.warning(f"Invalid data: {key}: {str(value_error)}")
-            print(f"WARNING: {key} has invalid data values")
+            logging.warn(f"Invalid data: {key}: {str(value_error)}")
+            print(f"WARNING: {key} has an invalid rental date value")
+
+        value['total_days'] = (rental_end - rental_start).days
+
+        if value['total_days'] < 0:
+            logging.error(f"Invalid data: {key} rental start " + \
+                f"{rental_start} is greater than the end {rental_end}")
+            print(f"ERROR: {key} rental start is greater than end")
+
+        try:
+            value['total_price'] = value['total_days'] * value['price_per_day']
+        except KeyError as key_err:
+            logging.warn(key_log.format(key, key_err.args[0], str(key_err)))
+            print(f"WARNING: {key} missing {key_err.args[0]} value")
+        except ValueError as value_error:
+            logging.warn(f"Invalid data: {key}: {str(value_error)}")
+            print(f"WARNING: {key} has invalid price_per_day")
+
+        try:
+            value['sqrt_total_price'] = math.sqrt(value['total_price'])
+        except ValueError as value_error:
+            logging.warn(f"Invalid data: {key}: {str(value_error)}")
+            print(
+                f"WARNING: {key} unable square root the negative total_price")           
+        
+        try:
+            value['unit_cost'] = value['total_price'] / value['units_rented']
+        except KeyError as key_err:
+            logging.warn(key_log.format(key, key_err.args[0], str(key_err)))
+            print(f"WARNING: {key} missing {key_err.args[0]} value")
+        except ValueError as value_error:
+            logging.warn(f"Invalid data: {key}: {str(value_error)}")
+            print(f"WARNING: {key} has invalid units_rented value")
         except ZeroDivisionError as zero_error:
-            logging.warning(f"Invalid data: {key}: units_rented is 0: {str(zero_error)}")
+            logging.warn(
+                f"Invalid data: {key}: units_rented is 0: {str(zero_error)}")
             print(f"WARNING: {key} units_rented cannot be 0")
 
 def save_to_json(filename, file_data):
