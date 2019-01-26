86d52a95fd8778bb49ae8153868d496a679257fe
Dianna Tingg <dtingg@uw.edu>
2019-01-26 15:17:55 -0800

    updated

--- students/DiannaTingg/lessons/lesson03/activity/personjobdept_add_people.py
+++ students/DiannaTingg/lessons/lesson03/activity/personjobdept_add_people.py
@@ -2,12 +2,13 @@
 Adding people to our database.
 """
 
-from personjobdept_model import *
+import personjobdept_model as pjd
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.info('Working with Person class')
+
+logger.info('Working with the Person class.')
 
 PERSON_NAME = 0
 LIVES_IN_TOWN = 1
@@ -21,29 +22,24 @@ people = [
     ('Steven', 'Colchester', None),
     ]
 
-logger.info('Creating Person records: iterate through the list of tuples')
-logger.info('Prepare to explain any errors with exceptions')
-logger.info('and the transaction tells the database to rollback on error')
-
 for person in people:
     try:
-        with database.transaction():
-            new_person = Person.create(
+        with pjd.database.transaction():
+            new_person = pjd.Person.create(
                     person_name=person[PERSON_NAME],
                     lives_in_town=person[LIVES_IN_TOWN],
                     nickname=person[NICKNAME])
             new_person.save()
-            logger.info('Database add successful')
+            logger.info('Database add successful.')
 
     except Exception as e:
         logger.info(f'Error creating = {person[PERSON_NAME]}')
         logger.info(e)
-        logger.info('See how the database protects our data')
 
 logger.info('Read and print all Person records we created...')
 
-for person in Person:
+for person in pjd.Person:
     logger.info(f'{person.person_name} lives in {person.lives_in_town} '
                 f'and likes to be known as {person.nickname}.')
 
-database.close()
+pjd.database.close()
