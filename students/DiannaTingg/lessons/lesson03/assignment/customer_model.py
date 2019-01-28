20c755a8d0dab2365919afeff6889dafe9358e28
Dianna Tingg <dtingg@uw.edu>
2019-01-27 18:18:16 -0800

    updated

--- students/DiannaTingg/lessons/lesson03/assignment/customer_model.py
+++ students/DiannaTingg/lessons/lesson03/assignment/customer_model.py
@@ -3,19 +3,19 @@ Define schema for customer model.
 """
 
 import logging
-from peewee import SqliteDatabase, Model, CharField, BooleanField, DecimalField
+import peewee as pw
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 logger.info('Connecting to a sqlite database.')
 
-database = SqliteDatabase('customers.db')
+database = pw.SqliteDatabase('customers.db')
 database.connect()
 database.execute_sql('PRAGMA foreign_keys = ON;')
 
 
-class BaseModel(Model):
+class BaseModel(pw.Model):
     class Meta:
         database = database
 
@@ -25,11 +25,11 @@ class Customer(BaseModel):
     Defining data fields for a customer.
     """
     logger.info('Defining the Customer class.')
-    customer_id = CharField(primary_key=True, max_length=30)
-    first_name = CharField(max_length=30)
-    last_name = CharField(max_length=30)
-    home_address = CharField(max_length=100)
-    phone_number = CharField(max_length=20)
-    email_address = CharField(max_length=100)
-    active_status = BooleanField()
-    credit_limit = DecimalField(max_digits=7, decimal_places=2)
+    customer_id = pw.CharField(primary_key=True, max_length=30)
+    first_name = pw.CharField(max_length=30)
+    last_name = pw.CharField(max_length=30)
+    home_address = pw.CharField(max_length=100)
+    phone_number = pw.CharField(max_length=20)
+    email_address = pw.CharField(max_length=100)
+    active_status = pw.BooleanField()
+    credit_limit = pw.DecimalField(max_digits=7, decimal_places=2)
