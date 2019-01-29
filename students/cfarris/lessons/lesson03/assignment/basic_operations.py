c962cbafa94b565e798fe11a391a93d576addea6
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2019-01-29 14:27:31 -0800

    Finalized/linted basic_operations.py

--- students/cfarris/lessons/lesson03/assignment/basic_operations.py
+++ students/cfarris/lessons/lesson03/assignment/basic_operations.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env Python3
 
-####Assignment 3###
-##basic operations for peewee
+'''
+This scripts provides the functions to interact with
+the sqlite3 database via peewee
+'''
 
-#Current progress: 
-#
 
 import logging
 from customer_model import Customer
 
 
-def add_customer(customer_id, first_name, last_name, home_address, phone_number, email_address, status, credit_limit):
+def add_customer(customer_id, first_name, last_name, home_address,
+                 phone_number, email_address, status, credit_limit):
     '''
      Adds customer to customer database
      PARAMS: Multiple fields. customer_id is primary key
@@ -49,9 +50,9 @@ def search_customer(customer_id):
                      'last_name': retr_customer.last_name,
                      'email_address': retr_customer.email_address,
                      'phone_number': retr_customer.phone_number}
-    except Exception as e:
-        logging.error(e)
-        print('Error occurred retrieving cust: ', e)
+    except Exception as err:
+        logging.error(err)
+        print('Error occurred retrieving cust: ', err)
         cust_dict = {}
         return cust_dict
     return cust_dict
@@ -64,8 +65,8 @@ def delete_customer(customer_id):
     try:
         person = Customer.get(Customer.customer_id == customer_id)
         person.delete_instance()
-    except Exception as e:
-        logging.error('error finding and deleting customer:', e)
+    except Exception as err:
+        logging.error('error finding and deleting customer:', err)
         print('Customer wasn\'t found in database. Database unchanged')
     print('person successfully deleted')
     return 'person successfully deleted'
@@ -78,26 +79,28 @@ def update_customer_credit(customer_id, credit_limit):
     A ValueError will be raised if customer does not exist in database.
     '''
     try:
-        #below isn't correct! Just a placeholder...
         retr_customer = Customer.get(Customer.customer_id == customer_id)
         retr_customer.credit_limit = credit_limit
         retr_customer.save()
         if credit_limit < 0:
-            logging.warning('credit_limit parameter is less than 0: ', credit_limit)
+            logging.warning('credit_limit parameter is ',
+                            'less than 0: ', credit_limit)
     except ValueError as err:
         logging.error('error replacing customers credit limit: ,', err)
         error_statement = 'An error occured while replacing customers credit.\
                          Credit unchanged.'
         print(error_statement)
         return error_statement
-    check_customer = Customer.get(Customer.customer_id == customer_id)    
-    print(check_customer.customer_id, ' has had their credit limit changed to:', check_customer.credit_limit)
+    check_customer = Customer.get(Customer.customer_id == customer_id)
+    print(check_customer.customer_id,
+          ' has had their credit limit changed to:',
+          check_customer.credit_limit)
     return 'operation successfully completed'
 
 
 def list_active_customers():
     '''
-    lists active customers
+    Returns the number of active customers
     '''
     count = 0
     for customer in Customer.select().where(Customer.status == True):
@@ -106,14 +109,5 @@ def list_active_customers():
     return count
 
 
-
 if __name__ == '__main__':
-
-    #added customer below already
-    #add_customer('2212','Jerry','Blank','19202 130th Ave, Sun City, Az. 98125', '123-456-6543', 'jb@aol.com', False, 500.00)    
-    print("already added customers. name == main is just a placeholder")
-    print(search_customer('2212'))  #function returns a dictionary
-    #search_customer('1') #confirming it will throw an error because it isn't in database.
-    #delete_customer('2212') #works correctly
-    update_customer_credit('2212', 20.00)
-    list_active_customers()
+    print('basic_operations.py is ready to be used with customers.db')
