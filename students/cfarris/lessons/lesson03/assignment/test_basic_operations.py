96da6c0ec9309a071a3792331c4831a1d546f1e3
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2019-01-29 13:29:07 -0800

    completed pytest test suite. I only have to complete linting. I will also try to do a unittest suite separately.

--- students/cfarris/lessons/lesson03/assignment/test_basic_operations.py
+++ students/cfarris/lessons/lesson03/assignment/test_basic_operations.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env Python3
 import pytest
-from basic_operations import add_customer, search_customer, delete_customer
+from basic_operations import add_customer, search_customer, delete_customer, update_customer_credit, list_active_customers
 from customer_model import Customer
 
 ##note, if you get an integrity error be sure clear your database. 
@@ -31,6 +31,16 @@ bad_customer = {'customer_id':'8675309',
                 'email_address': 'Jenny_Igotyonumber@gmail.com',
                 'status': True,
                 'credit_limit': 40.00}
+
+ok_customer_3 = {'customer_id':'A97654',
+                 'first_name': 'Jekyll',
+                 'last_name': 'Hyde',
+                 'home_address': 'Beverly Hills Plaza, Beverly Hills, Ca. 90210',
+                 'phone_number': '908-654-3333',
+                 'email_address': 'Jeklyll_Hyde@gmail.com',
+                 'status': True,
+                 'credit_limit': 40.00}
+
 
 
 def test_add_ok_customer():
@@ -68,9 +78,36 @@ def test_delete_customer():
     '''
     Tests delete_customer to verify specified customer was deleted from db
     First add the customer to be deleted, then delete them with joy!
+    Using ok_customer_2 for this test module only.
     '''
     add_customer(**ok_customer_2)
     test_customer_2 = Customer.get(Customer.customer_id == ok_customer_2['customer_id'])
     assert test_customer_2.email_address == ok_customer_2['email_address']
     delete_results = delete_customer(test_customer_2.customer_id)
     assert delete_results == 'person successfully deleted'
+
+
+def test_update_customer_credit():
+    '''
+    Tests update_customer_credit. 
+    '''
+    update_customer_credit(ok_customer['customer_id'], 75.00)
+    test_customer = Customer.get(Customer.customer_id == ok_customer['customer_id'])
+    assert test_customer.credit_limit == 75.00
+
+
+def test_list_active_customers():
+    '''
+    Test that list_active customers returns the amount of those active.
+    The way I chose to test this was to get an accurate count of active customers, 
+    add a new customerthat is active
+    Test to see that the active customers number has increased.
+    '''
+    number_of_customers= Customer.select().count()
+    num_active = list_active_customers()
+    add_customer(**ok_customer_3) #bad customer should work
+    num_active_2 = list_active_customers()
+    delete_customer(ok_customer_3['customer_id'])
+    assert (num_active + 1) == num_active_2
+
+
