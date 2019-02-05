797d31cc82e1b91ab12b59ec32ef30441f5eff95
Maria Mckinley <maria.k.mckinley@disney.com>
2019-02-04 22:01:39 -0800

    importing all of the data from the csv in my test now

--- students/Maria/lesson03/test_customer.py
+++ students/Maria/lesson03/test_customer.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import os
+import csv
 
 import config
 import basic_operations as bs
@@ -22,6 +23,10 @@ class TestCustomer(TestCase):
         database.connect()
         database.execute_sql('PRAGMA foreign_keys = ON;')  # needed for sqlite only
         create_cust_table()
+        # add a bunch of customers to make sure works with non-empty database
+        # maybe change this so it isn't quite so many, tests take too long now
+        bs.add_from_csv('customer.csv')
+        cls.active_cust_csv = get_count('customer.csv')
 
     @classmethod
     def tearDownClass(cls):
@@ -38,10 +43,13 @@ class TestCustomer(TestCase):
 
     def tearDown(self):
         """
-        Remove all customers so can start again
+        Remove customers from config so can start again (won't always have both)
         """
-        delete_cust_table() # changed where I put this. now it is in customer_model,
-        # where users are unlikely to have access, so doesn't have to be private
+        for customer in (config.customer1, config.customer2):
+            try:
+                bs.delete_customer(customer['customer_id'])
+            except ValueError:
+                pass
 
     def test_add_customer(self):
         bs.add_customer(**config.customer2)
@@ -50,11 +58,16 @@ class TestCustomer(TestCase):
 
     def test_add_customer_incomplete_data(self):
         bad_cust = 'elmer fudd'
-        with self.assertRaises(Exception) as exc:
+        with self.assertRaises(ValueError) as exc:
             bs.add_customer(bad_cust)
-        # I really wanted to raise a different exception instead,
-        # but this test hated that.
-        self.assertEqual(str(exc.exception), "'first_name'")
+        # problem was I couldn't add a message to a key error, so changed from keyerror to valueerror
+        self.assertEqual(str(exc.exception), config.etext['no_save'].format(bad_cust))
+
+    def test_customer_status_wrong(self):
+        with self.assertRaises(ValueError) as exc:
+            bs.add_customer(**config.bad_customer)
+        self.assertEqual(str(exc.exception),
+                         config.etext['req_bool'].format(config.bad_customer['status']))
 
     def test_search_customer(self):
         cust = bs.search_customer(config.customer1['customer_id'])
@@ -71,7 +84,8 @@ class TestCustomer(TestCase):
 
     def test_list_active_customers(self):
         result = bs.list_active_customers()
-        self.assertEqual(result, 1)
+        exp_result = 1 + self.active_cust_csv
+        self.assertEqual(result, exp_result)
 
     def test_delete_customer(self):
         bs.delete_customer(config.customer1['customer_id'])
@@ -83,3 +97,13 @@ class TestCustomer(TestCase):
         with self.assertRaises(Exception) as exc:
             bs.delete_customer(unk_id)
         self.assertEqual(str(exc.exception), config.etext['not_found'].format(unk_id))
+
+
+def get_count(csv_file):
+    """ Make sure what we count in the file is what the database says is true"""
+    count = 0
+    with open('customer.csv', 'r', newline='', encoding='ISO-8859-1') as myfile:
+        for row in csv.reader(myfile):
+            if row[6].lower() == 'active':
+                count += 1
+    return count
