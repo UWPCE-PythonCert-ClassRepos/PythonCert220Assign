b18ac48694ac6483285983c358436829363ae27e
Dianna Tingg <dtingg@uw.edu>
2019-02-10 15:30:47 -0800

    udpated

--- students/DiannaTingg/lessons/lesson05/assignment/test_database.py
+++ students/DiannaTingg/lessons/lesson05/assignment/test_database.py
@@ -32,63 +32,35 @@ def test_import_data(mongo_database):
     assert result == ((10, 10, 9), (0, 0, 0))
 
 
+def test_show_available_products(mongo_database):
+    d.import_data(mongo_database, "", "products.csv", "customers.csv", "rentals.csv")
+
+    result = d.show_available_products(mongo_database)
+
+    assert len(result) == 7
+    assert "prd001" in result
+    assert "prd002" not in result
 
 
-# def show_available_products(db):
-#     """
-#     Returns a dictionary for each product listed as available.
-#     :param db: MongoDB
-#     :return: Dictionary with product_id, description, product_type, quantity_available.
-#     """
-#
-#     available_products = {}
-#
-#     for product in db.products.find():
-#         if int(product["quantity_available"]) > 0:
-#
-#             product_dict = {"description": product["description"],
-#                             "product_type": product["product_type"],
-#                             "quantity_available": product["quantity_available"]}
-#
-#             available_products[product["product_id"]] = product_dict
-#
-#     return available_products
-#
-#
-# def show_rentals(db, product_id):
-#     """
-#     Returns a dictionary with user information from users who have rented products matching the product_id.
-#     :param db: MongoDB
-#     :param product_id: product id
-#     :return: user_id, name, address, phone_number, email
-#     """
-#
-#     customer_info = {}
-#
-#     for rental in db.rentals.find():
-#         if rental["product_id"] == product_id:
-#             customer_id = rental["user_id"]
-#
-#             customer_record = db.customers.find_one({"user_id": customer_id})
-#
-#             customer_dict = {"name": customer_record["name"],
-#                              "address": customer_record["address"],
-#                              "phone_number": customer_record["phone_number"],
-#                              "email": customer_record["email"]}
-#
-#             customer_info[customer_id] = customer_dict
-#
-#     return customer_info
-#
-#
-# def clear_data(db):
-#     """
-#     Delete data in MongoDB.
-#     :param db: MongoDB
-#     :return: Empty MongoDB.
-#     """
-#     db.products.drop()
-#     db.customers.drop()
-#     db.rentals.drop()
-#
-#
+def test_show_rentals(mongo_database):
+    d.import_data(mongo_database, "", "products.csv", "customers.csv", "rentals.csv")
+
+    result = d.show_rentals(mongo_database, "prd005")
+    assert len(result) == 2
+
+    print(result.keys())
+    assert list(result.keys()) == ["user001", "user003"]
+
+
+def test_clear_data(mongo_database):
+    d.import_data(mongo_database, "", "products.csv", "customers.csv", "rentals.csv")
+
+    result = mongo_database.list_collection_names()
+
+    assert result == ["products", "rentals", "customers"]
+
+    d.clear_data(mongo_database)
+
+    result2 = mongo_database.list_collection_names()
+
+    assert result2 == []
