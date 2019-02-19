84db83568c6388310318fea2f36dfd37121fdd70
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2019-02-19 11:06:52 -0800

    Lesson04 ready for review. Added batch CSV entry, functions to check status and customer credit. Linted to 7.52.

--- students/cfarris/lessons/lesson04/assignment/basic_operations.py
+++ students/cfarris/lessons/lesson04/assignment/basic_operations.py
@@ -4,20 +4,32 @@
 This scripts provides the functions to interact with
 the sqlite3 database via peewee
 '''
-
-import peewee as peewee
-import logging
-from customer_model import Customer
 import csv
-import sys
+import logging
+import peewee
+from customer_model import Customer
 
 
-log_format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
-formatter = logging.Formatter(log_format)  #create a formatter
-logger = logging.getLogger()  #calls the logger object
-file_handler = logging.FileHandler('basic_operations.log')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+'''
+Note: Config_db.py specifies which db to use in customer_model
+to run basic operations.py, make sure config_db.py has the 
+production database selected. Would love to hear how to better
+structure that.
+
+Pylint gave this a 7.52 rating. 
+iterator is for CSV.reader
+Yield is in test_basic operations
+I couldn't find a place for a comprehension without adding a list or dict
+just to add one.
+'''
+
+
+LOG_FORMAT = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+FORMATTER = logging.Formatter(LOG_FORMAT)  # create a formatter
+logger = logging.getLogger()  # calls the logger object
+FILE_HANDLER = logging.FileHandler('db.log')
+FILE_HANDLER.setFormatter(FORMATTER)
+logger.addHandler(FILE_HANDLER)
 
 
 def add_customer(customer_id, first_name, last_name, home_address,
@@ -32,7 +44,6 @@ def add_customer(customer_id, first_name, last_name, home_address,
     except ValueError as err:
         logging.error(err)
         raise  # Credit limit has to be a float to work in our database
-    
     try:
         new_customer = Customer.create(
             customer_id=customer_id,
@@ -44,12 +55,11 @@ def add_customer(customer_id, first_name, last_name, home_address,
             status=status,
             credit_limit=credit_limit)
         new_customer.save()
+        logging.info("%s successfully added to db.", new_customer)
     except peewee.IntegrityError as err:
-        logging.error(f"Error adding customer: {err},"
-                      f" {customer_id}, {first_name},"
-                      f"{last_name}")
+        logging.error("Error adding customer: \
+                      %s, %s", err, customer_id)
         print(f"Error adding customer: {customer_id}")
-        #raise
 
 
 def search_customer(customer_id):
@@ -81,8 +91,9 @@ def delete_customer(customer_id):
     try:
         person = Customer.get(Customer.customer_id == customer_id)
         person.delete_instance()
+        logging.info(f"{person} successfully deleted from db")
     except Exception as err:
-        logging.error('error finding and deleting customer:', err)
+        logging.error('error finding and deleting customer: %s', err)
         print('Customer wasn\'t found in database. Database unchanged')
     print('person successfully deleted')
     return 'person successfully deleted'
@@ -98,6 +109,8 @@ def update_customer_credit(customer_id, credit_limit):
         retr_customer = Customer.get(Customer.customer_id == customer_id)
         retr_customer.credit_limit = credit_limit
         retr_customer.save()
+        logging.info(f"customer credit changed {retr_customer.customer_id}\
+                     to credit {retr_customer.credit_limit}")
         if credit_limit < 0:
             logging.warning('credit_limit parameter is ',
                             'less than 0: ', credit_limit)
@@ -121,7 +134,6 @@ def list_active_customers():
     count = 0
     for customer in Customer.select().where(Customer.status == True):
         count = count + 1
-    #count + 1 for customer in Customer.select().where(Customer.status == True)
     print('There are ', count, ' active customers')
     return count
 
@@ -133,7 +145,6 @@ def batch_enter_customers_to_db(file):
     line_count = 0
     with open(file, 'r', encoding='unicode_escape') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        #line_count = 0
         for row in csv_reader:
             if line_count == 0:  # capture the headers
                 line_count = line_count + 1
@@ -141,7 +152,7 @@ def batch_enter_customers_to_db(file):
             else:
                 line_count = line_count + 1
                 str_row = [value.strip() for value in row]
-                status = check_customer_status(str_row[0], str_row[6].lower())
+                status = check_customer_status(str_row[6].lower())
                 credit_limit = convert_customer_credit_to_float(str_row[0], str_row[7].lower())
                 add_customer(str_row[0],
                              str_row[1],
@@ -151,10 +162,10 @@ def batch_enter_customers_to_db(file):
                              str_row[5],
                              status,
                              credit_limit)
-        return line_count 
+        return line_count
 
 
-def convert_customer_credit_to_float(customer_id,credit_limit):
+def convert_customer_credit_to_float(customer_id, credit_limit):
     '''
     Accepts customer id (for logging) and credit limit and checks
     credit_limit can be a float. Returns a logging Warning if credit_limit
@@ -164,54 +175,47 @@ def convert_customer_credit_to_float(customer_id,credit_limit):
     '''
     try:
         credit_limit = float(credit_limit)
-    except Exception as e:
-        logging.error('Error converting credit limit to float:', e, customer_id, credit_limit)
+        logging.info(f"customer credit changed {customer_id}, {credit_limit}")
+    except Exception as err:
+        logging.error('Error converting credit limit to float:', err, customer_id)
         print('Error converting credit limit to float:', customer_id, credit_limit)
         raise
     else:
         return credit_limit
 
 
-
-
-def check_customer_status(customer_id,customer_status):
+def check_customer_status(customer_status):
     '''
     Checks a customers reported status and attemts to
     give status a boolean value. If it cannot, it will throw an error
-    
     Param: customer_status
     Return: boolean value of customer status
     '''
     if customer_status == 'active':
         return True
-    elif customer_status == 'inactive':
+    if customer_status == 'inactive':
         return False
-    else:
-        logging.warning('bad formatting for Customer status: ', customer_id, customer_status)
-        print('bad formatting for Customer status: ', customer_id, customer_status)
-       
 
+    logging.warning("bad formatting for Customer \
+                    status:%s", customer_status)
+    print(f"bad formatting for Customer status:\
+          {customer_status}")
 
 
 if __name__ == '__main__':
-    OK_CUSTOMER = {'customer_id': 'ABCD@#',
-               'first_name': 'Edgar',
-               'last_name': 'Poe',
-               'home_address': '1266 South Corrine Court, Walnut Creek CA 98125',
-               'phone_number': '123-456-7891',
-               'email_address': 'ep@gmail.com',
-               'status': True,
-               'credit_limit': 40.00}
+    '''
+    Below is a simple test to ensure the program runs as expceted.
+    '''
     OK_CUSTOMER_2 = {'customer_id': ' 0U812',
-                 'first_name': 'TAMMY',
-                 'last_name': 'Faye',
-                 'home_address': '1266 South Corrine Court, Walnut Creek CA 98125',
-                 'phone_number': '123-456-7891',
-                 'email_address': 'Tammy_Faye@gmail.com',
-                 'status': True,
-                 'credit_limit': 4000.00}               
-    add_customer(**OK_CUSTOMER_2)                        
+                     'first_name': 'TAMMY',
+                     'last_name': 'Faye',
+                     'home_address': '1266 South Corrine Court, Walnut Creek CA 98125',
+                     'phone_number': '123-456-7891',
+                     'email_address': 'Tammy_Faye@gmail.com',
+                     'status': True,
+                     'credit_limit': 4000.00}
+    add_customer(**OK_CUSTOMER_2)
     print('basic_operations.py is ready to be used with customers.db')
     list_active_customers()
     delete_customer(' 0U812')
-    #batch_enter_customers_to_db('lesson04_assignment_data_customer.csv') #works
+    # batch_enter_customers_to_db('lesson04_assignment_data_customer.csv') #works
