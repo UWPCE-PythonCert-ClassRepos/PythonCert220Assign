3bcf45f32d0e5f9e642eb6ac3df55607ae7b6514
Dan Cornutt <dacornutt@gmail.com>
2019-02-19 16:30:34 -0800

    Refactored good_perf.py file. Next step to use Cython

--- lessons/lesson06/assignment/good_perf.pyx
+++ lessons/lesson06/assignment/good_perf.pyx
@@ -7,16 +7,11 @@ import datetime
 import csv
 
 
-def analyze(filename):
+cdef analyze(char filename):
     start = datetime.datetime.now()
-    with open(filename) as csvfile:
+    with open(filename, encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         new_ones = []
-        for row in reader:
-            lrow = list(row)
-            if lrow[5] > '00/00/2012':
-                new_ones.append((lrow[5], lrow[0]))
-
         year_count = {
             "2013": 0,
             "2014": 0,
@@ -25,21 +20,12 @@ def analyze(filename):
             "2017": 0,
             "2018": 0
         }
-
-        for new in new_ones:
-            if new[0][6:] == '2013':
-                year_count["2013"] += 1
-            if new[0][6:] == '2014':
-                year_count["2014"] += 1
-            if new[0][6:] == '2015':
-                year_count["2015"] += 1
-            if new[0][6:] == '2016':
-                year_count["2016"] += 1
-            if new[0][6:] == '2017':
-                year_count["2017"] += 1
-            if new[0][6:] == '2018':
-                year_count["2017"] += 1
-
+        for row in reader:
+            lrow = list(row)
+            if lrow[5] > '00/00/2012':
+                new_ones.append((lrow[5], lrow[0]))
+                yr = lrow[5][:4]
+                year_count[yr] += 1
         print(year_count)
 
     with open(filename) as csvfile:
@@ -56,6 +42,8 @@ def analyze(filename):
         end = datetime.datetime.now()
 
     return (start, end, year_count, found)
+
+
 
 
 def main():
