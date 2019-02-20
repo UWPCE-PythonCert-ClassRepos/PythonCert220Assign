bd68b84ee8e7dd2ee81ef373bddca845a4275b29
Jean-Baptiste Yamindi <jbyamindi@yahoo.fr>
2019-02-20 00:58:37 -0800

    my update assignment03

---
+++ students/Jean-Baptiste/lessons/lesson03/assignment03_solution_Update_JB/create_customers.py
@@ -0,0 +1,12 @@
+"""
+This is to create database using the Peewee ORM, sqlite and Python
+
+"""
+# from  customers_model import *
+import customers_model as cm
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info('Let us build the classes from the model in the database')
+cm.database.create_tables([cm.Customer])
+cm.database.close()
