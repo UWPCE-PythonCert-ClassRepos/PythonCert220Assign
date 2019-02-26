883c2c684df349db0d6031d9c7694798c902b018
D Smith <dsmith82@uw.edu>
2019-02-26 15:18:25 -0800

    Assignment 07 parallelization

---
+++ students/DrewSmith/lessons/lesson07/assignment/customers_model.py
@@ -0,0 +1,34 @@
+"""
+    Simple database example with Peewee ORM, sqlite and Python
+    Here we define the schema
+    Use logging for messages so they can be turned off
+"""
+
+
+import logging
+import peewee as pw
+import assignment.create_customers as cc
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+class BaseModel(pw.Model):
+    ''' Base class for all database models '''
+    class Meta:
+        ''' BaseModel Meta class '''
+        database = cc.get_database()
+
+class Customer(BaseModel):
+    """
+    This class defines Customer, which maintains customer details
+    """
+    customer_id = pw.CharField(primary_key=True, max_length=30)
+    first_name = pw.CharField(max_length=30)
+    last_name = pw.CharField(max_length=30)
+    home_address = pw.CharField(max_length=100)
+    phone_number = pw.CharField(max_length=20)
+    email_address = pw.CharField(max_length=100)
+    status = pw.BooleanField()
+    credit_limit = pw.FloatField()
+
+cc.get_database().create_tables([Customer])
