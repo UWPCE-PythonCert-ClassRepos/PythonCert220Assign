1d9b2ea699364065dd8847d2d8c056766b5aa78a
Brian Nisonger <vanulor@gmail.com>
2019-03-02 23:49:51 -0800

    Adding what I have so far

--- students/BrianNisonger/Lesson8/assignment/test_inventory.py
+++ students/BrianNisonger/Lesson8/assignment/test_inventory.py
@@ -9,21 +9,16 @@ def does_not_raise():
     yield
 
 
-@pytest.mark.parametrize("test_input,expected,expectation",[
-(("invoice01.csv", "Elisa Miles", "LR04", "Leather Sofa", 25),("Elisa Miles", "LR04", "Leather Sofa", "25.00"),does_not_raise()),
-(("invoice01.csv", "Edward Data", "KT78", "Kitchen Table", 10),("Edward Data", "KT78", "Kitchen Table", "10.00"),does_not_raise()),
-(("invoice01.csv", "Alex Gonzales", "Queen Mattress", 17),(),pytest.raises(IndexError))
+@pytest.mark.parametrize("test_input,expectation",[
+(("invoice01.csv", "Elisa Miles", "LR04", "Leather Sofa", 25),does_not_raise()),
+(("invoice01.csv", "Edward Data", "KT78", "Kitchen Table", 10),does_not_raise()),
+(("invoice01.csv", "Alex Gonzales", "Queen Mattress", 17),pytest.raises(IndexError))
 ])
 
 
 
 
-def test_add_furniture(test_input,expected,expectation):
+def test_add_furniture(test_input,expectation):
     with expectation:
         add_furniture(*test_input)
-        a=open(test_input[0],'rb')
-        lines=a.readlines()
-        if lines:
-            last_line = lines[-1]
-        print(last_line)    
-        assert False
+        
