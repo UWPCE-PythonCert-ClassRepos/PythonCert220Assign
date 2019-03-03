d2776280cc4aa48a8a4464be64dcc1c9da91b100
Brian Nisonger <vanulor@gmail.com>
2019-03-03 10:16:12 -0800

    Assignment 8

--- students/BrianNisonger/Lesson8/assignment/test_inventory.py
+++ students/BrianNisonger/Lesson8/assignment/test_inventory.py
@@ -4,21 +4,46 @@ import pytest
 from contextlib import contextmanager
 import csv
 
+
 @contextmanager
 def does_not_raise():
     yield
 
 
-@pytest.mark.parametrize("test_input,expectation",[
-(("invoice01.csv", "Elisa Miles", "LR04", "Leather Sofa", 25),does_not_raise()),
-(("invoice01.csv", "Edward Data", "KT78", "Kitchen Table", 10),does_not_raise()),
-(("invoice01.csv", "Alex Gonzales", "Queen Mattress", 17),pytest.raises(IndexError))
-])
+@pytest.fixture
+def test_single_customer_results():
+    return [["Susan Wong", "LR04", "Leather Sofa", "25.00"],
+            ["Susan Wong", "KT78", "Kitchen Table", "10.00"],
+            ["Susan Wong", "BR02", "Queen Mattress", "17.00"],
+            ["Susan Wong", "AA01", "Monkey Wrench", "18.00"],
+            ["Susan Wong", "JMLL", "Pumpkin Head", "19.00"]]
 
 
-
-
-def test_add_furniture(test_input,expectation):
+@pytest.mark.parametrize(
+    "test_input,expected,expectation",
+    [(("invoice01.csv", "Elisa Miles", "LR04", "Leather Sofa", 25),
+      b'Elisa Miles,LR04,Leather Sofa,25.00\r\r\n', does_not_raise()),
+     (("invoice01.csv", "Edward Data", "KT78", "Kitchen Table", 10),
+      b'Edward Data,KT78,Kitchen Table,10.00\r\r\n', does_not_raise()),
+     (("invoice01.csv", "Alex Gonzales", "Queen Mattress", 17), None,
+      pytest.raises(IndexError)),
+     (("invoice01.csv", "Elisa Miles", "LR04", "Leather Sofa", "a"), None,
+      pytest.raises(ValueError))])
+      
+def test_add_furniture(test_input, expected, expectation):
     with expectation:
         add_furniture(*test_input)
-        
+        with open(test_input[0], 'rb') as file:
+            lines = file.readlines()
+            if lines:
+                last_line = lines[-1]
+            assert expected == last_line
+
+
+def test_single_customer(test_single_customer_results):
+    create_invoice = single_customer("Susan Wong", "SW_invoice.csv")
+    create_invoice("test_items.csv")
+    with open("test_items.csv", mode="r") as customer_file:
+        customer_reader = csv.reader(customer_file)
+        customer_list = [row for row in customer_reader]
+    assert customer_list[0] == test_single_customer_results[0]
