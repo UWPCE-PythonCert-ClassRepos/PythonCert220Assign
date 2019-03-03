49a0f8dd4c65c7805110a81e76dc85681bdc1253
Jackie Cao <jackcao@uw.edu>
2019-03-03 11:13:14 -0800

    change for database development

--- students/ShujinCao/lesson05/assignment/database.py
+++ students/ShujinCao/lesson05/assignment/database.py
@@ -19,7 +19,7 @@ class MongoDBConnection():
 
 def list_renter_info(rented_product_id):
     mongo = MongoDBConnection()
-    renters = []
+    renters = {}
 
     with mongo:
         db = mongo.connection.media
@@ -27,20 +27,22 @@ def list_renter_info(rented_product_id):
         if rentals:
             for i in rentals:
                 renter = db.customers.find_one({"user_id": f"{i['user_id']}"})
-                reters.append(renter)
-        print(renters)
+                user_id = renter.pop('user_id')
+                renters[user_id] = {**renter}
+        return renters
 
 def list_available_products():
     mongo = MongoDBConnection()
-    output = []
+    output = {}
 
     with mongo:
         db = mongo.connection.media
         all_prod = db.products.find()
         for i in all_prod:
             if int(i["quantity_available"]) > 0:
-                output.append(i)
-        print(output)
+                p_id = i['product_id']
+                output[p_id] = {**i}
+    return output
 
 def main_upload_data(f_product, f_customer, f_rental):
 
@@ -60,16 +62,17 @@ def main_upload_data(f_product, f_customer, f_rental):
 
 
 def upload_data(x, data):
-    import pdb; pdb.set_trace()
     mongo = MongoDBConnection()
     result = []
 
     with mongo:
         for i in range(len(data)):
-            value = dict(data.iloc[i])
+            data = data.astype(str)
+            value = dict(data.loc[i])
             result.append(value)
         x.insert_many(result)
 
 if __name__ == "__main__":
-    main_upload_data("products.csv", "customers.csv", "rentals.csv")
-    list_available_products()
+    # main_upload_data("products.csv", "customers.csv", "rentals.csv")
+    print(list_available_products())
+    print(list_renter_info("prd005"))
