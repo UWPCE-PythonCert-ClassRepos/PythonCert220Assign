ae5de69d6799cc93ba7e1bc3b4e5facf2faad812
Jae Kim <kjae>
2019-03-06 23:30:04 -0800

    Updated inventory

--- students/JaeKim/lesson08/assignment/inventory.py
+++ students/JaeKim/lesson08/assignment/inventory.py
@@ -1,12 +1,15 @@
+import csv
+
 def add_furniture(invoice_file, customer_name, item_code, item_description, item_monthly_price):
-    with open(invoice_file, 'a') as file:
-        file.write("\n{},{},{},{}".format(customer_name, item_code, item_description, item_monthly_price))
+    with open(invoice_file, "a+", newline='') as file:
+        writer = csv.writer(file)
+        row = customer_name, item_code, item_description, item_monthly_price
+        writer.writerow(row)
 
 def single_customer(customer_name, invoice_file):
     def another_function(target_csv):
-        #print("add everything from {} to {} with name {}".format(target_csv, invoice_file.csv, customer_name))
         with open(invoice_file) as source_csv:
-            original_entries = [line.split(",") for line in source_csv]
+            original_entries = [line.split(",") for line in source_csv if line]
 
         with open(target_csv, 'a') as new_csv:
             for item in original_entries:
@@ -14,5 +17,9 @@ def single_customer(customer_name, invoice_file):
     return another_function
 
 if __name__ == "__main__":
+    add_furniture("invoice_file.csv", "Elisa Miles", "LR04", "Leather Sofa", "25.00")
+    add_furniture("invoice_file.csv", "Edward Data", "KT78", "Kitchen Table", "10.00")
+    add_furniture("invoice_file.csv", "Alex Gonzales", "BR02", "Queen Mattress", "17.00")
+
     create_invoice = single_customer("Susan Wong", "invoice_file.csv")
     create_invoice("test_items.csv")
